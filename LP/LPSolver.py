2b6b89cbccca63a613964ec0e63015ebe2cd1d3f
Dung Nguyen <dungn@virginia.edu>
2019-10-15 12:01:58 -0400

    Small edit

--- LP/LPSolver.py
+++ LP/LPSolver.py
@@ -1,10 +1,10 @@
-import gurobipy as gu
+import gurobipy as grb
 
 # Load a model
 
 print("Loading a model")
 
-model = gu.read("/apps/software/vendor/gurobi/8.0.1/examples/data/afiro.mps")
+model = grb.read("/apps/software/vendor/grbrobi/8.0.1/examples/data/afiro.mps")
 
 model.optimize()
 
@@ -12,16 +12,16 @@ model.printAttr('X')
 
 print("Creating a new model")
 
-m = gu.Model()
+m = grb.Model()
 
 # Variables
-x = m.addVar(vtype=gu.GRB.BINARY, name = "x")
-y = m.addVar(vtype=gu.GRB.BINARY, name = "y")
-z = m.addVar(vtype=gu.GRB.BINARY, name = "z")
+x = m.addVar(vtype=grb.GRB.BINARY, name = "x")
+y = m.addVar(vtype=grb.GRB.BINARY, name = "y")
+z = m.addVar(vtype=grb.GRB.BINARY, name = "z")
 
 
 # Objectives
-m.setObjective(x + y + 2 * z, gu.GRB.MAXIMIZE)
+m.setObjective(x + y + 2 * z, grb.GRB.MAXIMIZE)
 
 # Constraints
 c1 = m.addConstr(2 * x + y + 3 * z <= 4)
---
+++ data_graphs/download_graphs.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+wget "http://snap.stanford.edu/data/ca-GrQc.txt.gz"
+gunzip -df ca-GrQc.txt.gz
