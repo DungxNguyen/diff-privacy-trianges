5614285dfcd7d7d459b6fa806b3d4fdd2edf9052
Dung Nguyen <dungn@virginia.edu>
2019-10-24 23:22:04 -0400

    finish skeleton of algorithm

--- Triangles/young.py
+++ Triangles/young.py
@@ -7,7 +7,9 @@ import math
 import random
 import shiva
 
+
 network_path = "../data_graphs/ca-GrQc.txt"
+
 
 def triangle_count(net):
     triangles = {}
@@ -42,7 +44,7 @@ def sum_triangles_of_node(i, nodes, X):
     return sum_x
 
 
-def normP(nodes, X, N):
+def normP(nodes, X, N, D):
     triangle_norm = np.sum(np.exp(X * N))
 
     node_norm = 0
@@ -57,43 +59,76 @@ def partialP(j, nodes, triangles, X, N, D):
     node_sum = 0
 
     for node in triangles[j]:
-        node_sum += (2 * N / (D * (D -1 ))) * \
-            np.exp((2 * N / (D * (D -1 ))) * \
+        node_sum += (2 * N / (D * (D - 1))) * \
+            np.exp((2 * N / (D * (D - 1))) * \
                    sum_triangles_of_node(node, nodes, X))
 
-    return triangle_sum + node_sum
+    return (triangle_sum + node_sum)
 
 
 def young_triangle_count(net, c, D, epsilon):
     (count, triangles, nodes) = triangle_count(net)
     T = count
-    N = 2 * math.log(T + net.number_of_nodes() + 1)
+    N = (2 * math.log(T + net.number_of_nodes() + 1)) / epsilon
     alpha = epsilon / N
     X = np.zeros(T)
 
-    while np.amin( N / c * X) < N:
-        for j in range(T):
-            ratio_j = partialP(j, nodes, triangles, X, N, D) / (N / c)
-            if ratio_j < 1 + epsilon:
-                break
+    print("N: ", N)
 
+    iter_count = 0
+    while np.sum(X) < c:
+        j_select = -1
+        infeasible = True
+        for j in range(T):
+            norm = normP(nodes, X, N, D)
+            ratio_j = partialP(j, nodes, triangles, X, N, D) / norm / (N / c)
+            # print("partial", j, ": ", partialP(j, nodes, triangles, X, N, D))
+            # print("ratio", j, ": ", ratio_j)
+            if infeasible and ratio_j <= 1:
+                infeasible = False
+            if ratio_j <= 1 + epsilon:
+                j_select = j
+                if not infeasible:
+                    break
+
+        if infeasible:
+            return -1
         # Add alpha j to X
-        X[j] += alpha
+        iter_count += 1
+        X[j_select] += alpha
+        # print("Iter: ", iter_count)
+        # print("j index: ", j_select)
+        # print("X: ", X)
+
+        if (iter_count % 10 == 0):
+            print("Iter ", iter_count, ": ", np.sum(X))
 
     print("Real Count: ", count)
     # print("Triangles: ", triangles.items())
     # print("Nodes: ", nodes.items())
-    
-    return 0
-    
+
+    # TODO Fix the return value here
+    return np.sum(X)
+
 
 def main():
     net = nx.read_edgelist(network_path, create_using=nx.Graph(), nodetype=int)
 
-    young_count = young_triangle_count(net, 50, 1):
+    n_node = 10
+    d_bound = 5
 
+    net = nx.random_regular_graph(d_bound + 1, n_node)
+
+    print("Nodes: ", net.number_of_nodes())
+    count, triangles, nodes = triangle_count(net)
+    print("Triangles: ", count)
+
+    #net = nx.Graph()
+    #net.add_edges_from([(1, 2), (1, 3), (2, 3)])
+
+    young_count = young_triangle_count(net, count, d_bound, 2)
     print("Young count: ", young_count)
-    
+
 
 if __name__ == "__main__":
     main()
