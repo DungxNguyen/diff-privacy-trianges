a95f136b4f13de21f13a43060d43a6be0783a936
Dung Nguyen <dungn@virginia.edu>
2019-10-26 00:09:49 -0400

    Add SciPy Linear Programming solver to Shiva alg

--- Triangles/shiva.py
+++ Triangles/shiva.py
@@ -3,7 +3,9 @@
 import networkx as nx
 import gurobipy as grb
 import numpy as np
+import scipy.optimize as opt
 import math
+import timeit
 
 network_path = "../data_graphs/ca-GrQc.txt"
 
@@ -20,7 +22,47 @@ def list_triangles(net):
 
     return list_of_triangles
 
-def linear_program_solve(net, D):
+
+def linear_program_solve(net, D, method="gurobi"):
+
+    if method == "gurobi":
+        return linear_program_solve_gurobi(net, D)
+
+    return linear_program_solve_scipy(net, D)
+
+
+def linear_program_solve_scipy(net, D):
+
+    triangles = list_triangles(net)
+
+    num_triangles = len(triangles)
+
+    number_of_nodes = net.number_of_nodes()
+
+    c = np.ones(num_triangles) * -1
+
+    P = np.zeros((number_of_nodes, num_triangles))
+
+    p = np.ones(number_of_nodes) * (D * (D-1) / 2)
+
+    for i in range(number_of_nodes):
+        for j in range(num_triangles):
+            if i in triangles[j]:
+                P[i][j] = 1
+
+    start = timeit.default_timer()
+
+    lp = opt.linprog(c, A_ub=P, b_ub=p, bounds=(0, 1))
+
+    end = timeit.default_timer()
+    duration = end - start
+
+    print("LP Solver time: ", duration)
+
+    return -lp.fun
+
+
+def linear_program_solve_gurobi(net, D):
     triangles = list_triangles(net)
 
     # Linear Programming Model
@@ -48,7 +90,7 @@ def linear_program_solve(net, D):
     return lpm.ObjVal
 
 
-def shiva_triange_count(net, D, epsilon):
+def shiva_triange_count(net, D, epsilon, method="gurobi"):
     real_triangle_count = total_triangles(net)
 
     number_of_nodes = net.number_of_nodes()
@@ -62,7 +104,8 @@ def shiva_triange_count(net, D, epsilon):
     if f1_hat > 7 * threshold:
         return f1_hat
 
-    lpm = linear_program_solve(net, D)
+    lpm = linear_program_solve(net, D, method)
+    print("LP Count: ", lpm)
     noise = np.random.laplace(0, 6 * D ** 2 / epsilon)
     f2_hat = lpm + noise
 
@@ -78,11 +121,17 @@ def total_triangles(G):
 def main():
     net = nx.read_edgelist(network_path, create_using=nx.Graph(), nodetype=int)
 
-    # shiva algorithm with D = 50
-    shiva_count = shiva_triange_count(net, 50, 1)
+    n_node = 5000
+    edge_prob = 0.0125
 
+    net = nx.fast_gnp_random_graph(n_node, edge_prob)
     # real count by networkx
     print("Real count triangles: ", total_triangles(net))
+
+    d_bound = max(val for (node, val) in net.degree())
+    # shiva algorithm with D = 50
+    shiva_count = shiva_triange_count(net, d_bound, 1, "scipy")
+
 
     print("Shiva alg count: ", shiva_count)
 
